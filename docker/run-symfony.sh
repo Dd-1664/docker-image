a06d0e0d3dafbc8bdd89f9e3d5815c973ab8cd6f
Dd-1664 <david.dabo.pro@gmail.com>
2018-03-11 23:25:52 +0100

    rework script to convert endline from DOS to UNIX

--- docker/run-symfony.sh
+++ docker/run-symfony.sh
@@ -1,27 +1,41 @@
 #!/usr/bin/env bash
-set -e
+set -xe
+#sed $'s/\r$//'
+
 cd /var/www/html
 
 
-# prepare chmod
+# prepare chmod default values
 if [[ "$APACHE_USER_ID" -eq "" ]]; then
     APACHE_USER_ID="www-data"
 fi
+if [[ "$APACHE_GROUP_ID" -eq "" ]]; then
+    APACHE_GROUP_ID="$APACHE_USER_ID"
+fi
+
+# Tweak values for dev
+if [[ "$APACHE_GROUP_ID" -ne "www-data" ]]; then
+   echo "Tweak apache group id to $APACHE_GROUP_ID"
+   groupmod -g $APACHE_GROUP_ID www-data
+fi
+
 if [[ "$APACHE_USER_ID" -ne "www-data" ]]; then
     echo "Tweak apache user id to $APACHE_USER_ID"
     usermod -u $APACHE_USER_ID www-data
-
-    echo "Disable opcache"
-    sed -ie 's/opcache.enable=1/opcache.enable=0/g' /etc/php/7.1/apache2/conf.d/php.ini
 fi
 
 # fix cache chmod
-mkdir -p var/cache && mkdir -p var/logs
-chown -R $APACHE_USER_ID:$APACHE_USER_ID /var/www/html
-chmod -R 777 /var/www/html/var/cache
-chmod -R 777 /var/www/html/var/logs
-
+if [[ -d var/cache ]]; then
+    mkdir -p var/logs
+    chmod -R 777 var/cache
+    chmod -R 777 var/logs
+fi
 
 # We are all set, run apache in foreground
 echo "run apache2"
+
+if [[ $# -gt 0 ]]; then
+    cd /var/www/html
+	exec "$@"
+fi
 exec /usr/sbin/apache2ctl -D FOREGROUND
